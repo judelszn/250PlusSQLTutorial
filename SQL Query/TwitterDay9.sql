21a311964710f00057bce233e14faa718265c911
judelszn <judelszn@gmail.com>
2024-06-30 15:08:37 +0000

    TwitterDay9 SQL file update

--- SQL Query/TwitterDay9.sql
+++ SQL Query/TwitterDay9.sql
@@ -21,4 +21,36 @@ WHERE R.RetweetID IS NULL
 
 
 
--- Scenario Q18 - Analyse Engagement Metrics of Mentioned UsersWITH MentionedUsers AS (	SELECT M.MentionedUserID AS UserID	, COUNT(*) AS MentionCount	FROM Twitter.Mentions M	GROUP BY M.MentionedUserID	ORDER BY COUNT(*) DESC	OFFSET 0 ROWS	FETCH NEXT 5 ROWS ONLY	)SELECT MU.UserID	, U.Username	, U.FullName	, MU.MentionCount	, AVG(LK.LikeCount) AS AverageLikes	, AVG(RT.RetweetCount) AS AverageRetweetsFROM MentionedUsers MUINNER JOIN Twitter.Users U ON MU.UserID = U.UserIDLEFT JOIN (	SELECT L.UserID		, COUNT(*) AS LikeCount	FROM Twitter.Likes L	GROUP BY L.UserID	) LK ON LK.UserID = MU.UserIDLEFT JOIN (	SELECT R.UserID		, COUNT(*) AS RetweetCount	FROM Twitter.Retweets R	GROUP BY R.UserID	) RT ON RT.UserID = MU.UserIDGROUP BY MU.UserID, U.Username, U.FullName, MU.MentionCountORDER BY MU.MentionCount DESC;
+-- Scenario Q18 - Analyse Engagement Metrics of Mentioned Users
+WITH MentionedUsers AS (
+	SELECT M.MentionedUserID AS UserID
+	, COUNT(*) AS MentionCount
+	FROM Twitter.Mentions M
+	GROUP BY M.MentionedUserID
+	ORDER BY COUNT(*) DESC
+	OFFSET 0 ROWS
+	FETCH NEXT 5 ROWS ONLY
+	)
+SELECT MU.UserID
+	, U.Username
+	, U.FullName
+	, MU.MentionCount
+	, AVG(LK.LikeCount) AS AverageLikes
+	, AVG(RT.RetweetCount) AS AverageRetweets
+FROM MentionedUsers MU
+INNER JOIN Twitter.Users U ON MU.UserID = U.UserID
+LEFT JOIN (
+	SELECT L.UserID
+		, COUNT(*) AS LikeCount
+	FROM Twitter.Likes L
+	GROUP BY L.UserID
+	) LK ON LK.UserID = MU.UserID
+LEFT JOIN (
+	SELECT R.UserID
+		, COUNT(*) AS RetweetCount
+	FROM Twitter.Retweets R
+	GROUP BY R.UserID
+	) RT ON RT.UserID = MU.UserID
+GROUP BY MU.UserID, U.Username, U.FullName, MU.MentionCount
+ORDER BY MU.MentionCount DESC
+;
